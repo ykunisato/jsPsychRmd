b2a53344d59e75c1a5b740a1169d430d1712bafc
ykunisato <ykunisato@psy.senshu-u.ac.jp>
2020-05-03 21:41:56 +0000

    add pavlovia function

--- R/jsPsychRmd.R
+++ R/jsPsychRmd.R
@@ -7,30 +7,39 @@
 #' @examples # set_jsPsych("stroop")
 #' @export
 
-set_jsPsych <- function (file_name = "task", pavlovia = FALSE){
-  path = getwd()
-  dir.create(file.path(path, file_name), showWarnings = FALSE)
+set_jsPsych <- function (file_name = "task", folder = TRUE, pavlovia = FALSE){
+  if(folder == TRUE){
+    path = file.path(getwd(), file_name)
+    dir.create(path, showWarnings = FALSE)
+  }else{
+    path = getwd()
+  }
   # make stimuli directory
-  if(!dir.exists(file.path(path, file_name, "stimuli"))){
-    dir.create(file.path(path, file_name, "stimuli"), showWarnings = FALSE)
-    file.create(file.path(path, file_name, "stimuli/README_materials.md"), showWarnings = FALSE)
-    writeLines("README about stimuli", file.path(path, file_name, "stimuli/README_materials.md"))
+  if(!dir.exists(file.path(path, "stimuli"))){
+    dir.create(file.path(path, "stimuli"), showWarnings = FALSE)
+    file.create(file.path(path, "stimuli/README_materials.md"), showWarnings = FALSE)
+    writeLines("README about stimuli", file.path(path, "stimuli/README_materials.md"))
   }
   # make jsPsych directory
   dir_jsPsych <- "jspsych-6.1.0"
-  if(!dir.exists(file.path(path, file_name, dir_jsPsych))){
+  if(pavlovia == TRUE){
+    path_jsPsych <- file.path(path, "lib/vendors", dir_jsPsych)
+  }else{
+    path_jsPsych <- file.path(path, dir_jsPsych)
+  }
+  if(!dir.exists(path_jsPsych)){
     temp <- tempfile()
     download.file("https://github.com/jspsych/jsPsych/releases/download/v6.1.0/jspsych-6.1.0.zip",temp)
-    dir.create(paste0(path,"/",file_name,"/",dir_jsPsych))
-    unzip(temp, exdir = file.path(path, file_name, dir_jsPsych))
+    dir.create(path_jsPsych, showWarnings = FALSE)
+    unzip(temp, exdir = path_jsPsych)
     unlink(temp)
   }
   # make RMarkdown file and directory
-  if(!file.exists(file.path(path, file_name, paste0("/",file_name,".Rmd")))){
+  if(!file.exists(file.path(path, "index.Rmd"))){
     if(pavlovia == TRUE){
-      rmarkdown::draft(paste0(file_name,"/",file_name,".Rmd"), template = "pavlovia", package = "jsPsychRmd", edit = FALSE)
+      rmarkdown::draft(file.path(path, "index.Rmd"), template = "pavlovia", package = "jsPsychRmd", edit = FALSE)
     }else{
-      rmarkdown::draft(paste0(file_name,"/",file_name,".Rmd"), template = "jsPsych", package = "jsPsychRmd", edit = FALSE)
+      rmarkdown::draft(file.path(path, "index.Rmd"), template = "jsPsych", package = "jsPsychRmd", edit = FALSE)
     }
   }
 }
