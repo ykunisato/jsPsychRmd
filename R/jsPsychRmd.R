15942fc0e7a664ce492d91acd3a6fbe4bd407463
ykunisato <ykunisato@psy.senshu-u.ac.jp>
2021-04-10 21:57:12 +0000

    update

--- R/jsPsychRmd.R
+++ R/jsPsychRmd.R
@@ -1,20 +1,34 @@
-#' Set the Rmd file and directory for jsPsych
-#'
-#' @param pavlovia If you set TRUE, set_jsPsych set Rmd for conducting the task in Pavlova (set FALSE in default)
+#' @title Set the Rmd file and directory for jsPsych
+#' @importFrom utils download.file
+#' @importFrom utils unzip
+#' @importFrom rmarkdown draft
+#' @param jsPsych_version If you set a specific version number of jsPsych,
+#'                set_jsPsych prepare a file with that version of jsPsych.
+#' @param psychophysics_version If you set version number, set_jsPsych prepare a file of jspsych-psychophysics(set FALSE in default)
 #' @param folder If you set TRUE, set_jsPsych make directory that you set and put file in it(set FALSE in default)
 #' @param exclude_smartphone If you set TRUE, set_jsPsych download the platform.js and set Rmd file(set FALSE in default)
-#' @return Make directories for Research Compendium of CCP Lab and
-#' R Markdown file for thesis at Department of Psychology, Senshu University
+#' @param pavlovia If you set TRUE, set_jsPsych set Rmd for conducting the task in Pavlova (set FALSE in default)
+#' @param rc If you set name of folder of RC, set_jsPsych make directory of RC.
 #' @examples # set_jsPsych("stroop")
 #' @export
+set_jsPsych <- function (folder = FALSE,
+                         jsPsych_version = "6.3.1",
+                         psychophysics_version = FALSE,
+                         exclude_smartphone = FALSE,
+                         pavlovia = FALSE,
+                         rc = FALSE){
+  tmp_wd <- getwd()
+  if(rc != FALSE){
+    tmp_wd <- paste0(tmp_wd,"/",rc)
+  }
 
-set_jsPsych <- function (pavlovia = FALSE, folder = FALSE, exclude_smartphone = FALSE){
   if(folder != FALSE){
     path = file.path(getwd(), folder)
     dir.create(path, showWarnings = FALSE)
   }else{
     path = getwd()
   }
+
   # make stimuli directory
   if(!dir.exists(file.path(path, "stimuli"))){
     dir.create(file.path(path, "stimuli"), showWarnings = FALSE)
@@ -22,30 +36,42 @@ set_jsPsych <- function (pavlovia = FALSE, folder = FALSE, exclude_smartphone = 
     writeLines("README about stimuli", file.path(path, "stimuli/README_materials.md"))
   }
   # make jsPsych directory
-  dir_jsPsych <- "jspsych-6.1.0"
+  dir_jsPsych <- paste0("jspsych-",jsPsych_version)
   path_jsPsych <- file.path(path, dir_jsPsych)
   if(!dir.exists(path_jsPsych)){
     temp <- tempfile()
-    download.file("https://github.com/jspsych/jsPsych/releases/download/v6.1.0/jspsych-6.1.0.zip",temp)
+    download.file(paste0("https://github.com/jspsych/jsPsych/releases/download/v",jsPsych_version,"/jspsych-",jsPsych_version,".zip"),temp)
     dir.create(path_jsPsych, showWarnings = FALSE)
     unzip(temp, exdir = path_jsPsych)
     unlink(temp)
+  }
+  # make psychophysics directory
+  if(psychophysics_version != FALSE){
+    dir_psychophysics <- paste0("jspsych-psychophysics-",psychophysics_version)
+    path_psychophysics <- file.path(path, psychophysics_version)
+    if(!dir.exists(path_psychophysics)){
+      temp2 <- tempfile()
+      download.file(paste0("https://github.com/kurokida/jspsych-psychophysics/archive/refs/tags/v",psychophysics_version,".zip"),temp2)
+      dir.create(path_psychophysics, showWarnings = FALSE)
+      unzip(temp2, exdir = path_psychophysics)
+      unlink(temp2)
+    }
   }
   # make RMarkdown file and directory
   if(!file.exists(file.path(path, "index.Rmd"))){
     if(pavlovia == TRUE){
       if(exclude_smartphone == TRUE){
         download.file('https://raw.githubusercontent.com/bestiejs/platform.js/master/platform.js', destfile = file.path(path_jsPsych,"platform.js"), method = "wget")
-        rmarkdown::draft(file.path(path, "index.Rmd"), template = "pavlovia_exclude_smartphone", package = "jsPsychRmd", edit = FALSE)
+        draft(file.path(path, "index.Rmd"), template = "pavlovia_exclude_smartphone", package = "jsPsychRmd", edit = FALSE)
       }else{
-        rmarkdown::draft(file.path(path, "index.Rmd"), template = "pavlovia", package = "jsPsychRmd", edit = FALSE)
+        draft(file.path(path, "index.Rmd"), template = "pavlovia", package = "jsPsychRmd", edit = FALSE)
       }
     }else{
       if(exclude_smartphone == TRUE){
         download.file('https://raw.githubusercontent.com/bestiejs/platform.js/master/platform.js', destfile = file.path(path_jsPsych,"platform.js"), method = "wget")
-        rmarkdown::draft(file.path(path, "index.Rmd"), template = "jsPsych_exclude_smartphone", package = "jsPsychRmd", edit = FALSE)
+        draft(file.path(path, "index.Rmd"), template = "jsPsych_exclude_smartphone", package = "jsPsychRmd", edit = FALSE)
       }else{
-        rmarkdown::draft(file.path(path, "index.Rmd"), template = "jsPsych", package = "jsPsychRmd", edit = FALSE)
+        draft(file.path(path, "index.Rmd"), template = "jsPsych", package = "jsPsychRmd", edit = FALSE)
       }
     }
   }
